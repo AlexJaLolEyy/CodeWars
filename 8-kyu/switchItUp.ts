6191f516f7d90231cce42cbaa3d2580885d1e9a3
Alex <alexanderbenski@gmail.com>
2024-02-10 21:32:30 +0100

    ✨ adds solution for switchItUp

---
+++ 8-kyu/switchItUp.ts
@@ -0,0 +1,5 @@
+export function switchItUp(intNumber: number): string {
+    const stringNumbers = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+    return stringNumbers[intNumber]
+}
+switchItUp(1)
